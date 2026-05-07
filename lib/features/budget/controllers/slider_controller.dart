f7e32a9240ce6d7437465fc9f5bbacf1e7abf670
Youssef Mohamed Abd alhamid <11410120240709@stud.cu.edu.eg>
2026-05-07 21:59:57 +0300

    nothing

--- lib/features/budget/controllers/slider_controller.dart
+++ lib/features/budget/controllers/slider_controller.dart
@@ -1,6 +1,6 @@
 import 'package:carousel_slider/carousel_controller.dart';
 import 'package:get/get.dart';
-
+///////////
 class SliderController extends GetxController {
   static SliderController get instance => Get.find();
 
